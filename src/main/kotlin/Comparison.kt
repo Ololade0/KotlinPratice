a90ff9eadbf4e924d0a46f19e1004bf93a50af63
Ololade0 <adesuyiololade@gmail.com>
2022-11-02 14:17:58 +0100

    kotlin pratice

---
+++ src/main/kotlin/Comparison.kt
@@ -0,0 +1,23 @@
+fun main() {
+    val number = 100
+    val secondNumber = 10
+
+    if(number == secondNumber){
+        println("It is equal to eachother")
+    }
+
+
+    if(number != secondNumber){
+        println("It is not equal  to eachother")
+    }
+
+    if(number >= secondNumber){
+        println("It is greater than each other")
+    }
+
+    if(number <= secondNumber){
+        println("It is lesser than each other")
+    }
+
+}
+
---
+++ src/main/kotlin/MyName.kt
@@ -0,0 +1,49 @@
+fun main(){
+    println("Hi Ololade")
+
+
+    var language = "French"
+    val  score = 95
+    val name = "Ololade"
+    println(name)
+
+ val number = 100
+    val secondNumber = 10
+
+    val addition = number + secondNumber
+    println("The addition is $addition")
+
+
+    val subtraction = number - secondNumber
+    println("The difference is $subtraction")
+
+    val division = number / secondNumber
+    println("The division is $division")
+
+    val multiplication = number  * secondNumber
+    println("The Multiplication is $multiplication")
+
+
+    val a = 1
+    val b = true
+    var c  = 1
+    var result : Int
+    var booleanResult: Boolean
+    result = -a
+    println("-a = $result")
+    booleanResult = !b
+
+    println("!b = $booleanResult")
+
+    --c
+
+    println("--c = $c")
+
+
+
+
+
+
+
+
+}
